ad471ae560bf5a2b6110ab33ef3630661baeb0fb
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-03-02 00:25:03 +0900

    시큐리티 권한처리

--- src/main/java/metacoding/security/controller/IndexController.java
+++ src/main/java/metacoding/security/controller/IndexController.java
@@ -3,6 +3,8 @@ package metacoding.security.controller;
 import lombok.RequiredArgsConstructor;
 import metacoding.security.model.User;
 import metacoding.security.repository.UserRepository;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -55,5 +57,17 @@ public class IndexController {
         user.setPassword(encPassword);
         userRepository.save(user); // 회원가입 잘됨. 비밀번호:1234 => 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되었기 때문
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "개인정보";
     }
 }
