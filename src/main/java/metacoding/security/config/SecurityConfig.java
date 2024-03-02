801a67cb5eb305a95986651d2b361e1125d68153
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-03-02 13:11:42 +0900

    구글 회원 프로필 정보 받아오기

--- src/main/java/metacoding/security/config/SecurityConfig.java
+++ src/main/java/metacoding/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package metacoding.security.config;
 
+import metacoding.security.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,10 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 구글 로그인 완료된 뒤의 후처리가 필요함.
+// 1. 코드받기(인증됨)
+// 2. 엑세스 토큰(사용자 정보에 접근할 권한받음)
+// 3. 사용자 프로필 정보를 가져오고
+// 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+// 4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소), 백화점몰 -> (vip등급, 일반등급)
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // 해당 메서드의 리턴되는 오브젝트를 IOC로 등록해준다.
     @Bean
@@ -40,7 +51,10 @@ public class SecurityConfig {
 
         http.oauth2Login(form -> {
             form
-                    .loginPage("/loginForm"); // 구글 로그인 완료된 뒤의 후처리가 필요함
+                    .loginPage("/loginForm")
+                    .userInfoEndpoint(userInfoEndpointConfig -> {
+                        userInfoEndpointConfig.userService(principalOauth2UserService);
+                    }); // 구글 로그인 완료된 뒤의 후처리가 필요함. Tip. 코드X(엑세스토큰 + 사용자프로필정보 O)
         });
 
         return http.build();
--- src/main/java/metacoding/security/model/User.java
+++ src/main/java/metacoding/security/model/User.java
@@ -19,6 +19,10 @@ public class User {
     private String password;
     private String email;
     private String role;
+
+    private String provider;
+    private String providerId;
+
     @CreationTimestamp
     private Timestamp timestamp;
 }
